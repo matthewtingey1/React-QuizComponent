14728affa02f7dbb541389d73998c5424d14a843
matthewtingey1 <matthewtingey1@mail.weber.edu>
2021-08-15 18:28:45 -0600

    module 3 commit

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion.js'
 
 let quizData = require('./quiz_data.json')
 
@@ -9,9 +10,9 @@ class Quiz extends Component {
   }
   render() {
     return (
-      <div>
+      <QuizQuestion className = "QuizQuestion" quiz_question={{quiz_question: quizData.quiz_questions[this.state.quiz_position - 1]}}>
         <div className = "QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
-      </div>
+      </QuizQuestion>
     )
   }
 }
